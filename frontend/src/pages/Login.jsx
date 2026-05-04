c8d1d4b9a155bcda1dab66a4694fa704a59104fc
Hadi Gunawan <jtgunawan007@gmail.com>
2026-05-05 00:10:46 +0700

    update bgcolor login and register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -215,7 +215,7 @@ const Login = () => {
       </div>
 
       {/* RIGHT PANEL - Form Side */}
-      <div className="flex-1 flex items-center justify-center p-6 lg:p-12 xl:p-24 relative z-10 bg-slate-50 dark:bg-slate-950/20">
+      <div className="flex-1 flex items-center justify-center p-6 lg:p-12 xl:p-24 relative z-10 bg-slate-200 dark:bg-slate-950/20">
 
         <div className="w-full max-w-sm relative">
           <div className="bg-white dark:bg-white/5 rounded-3xl border border-slate-200 dark:border-white/10 overflow-hidden p-8">
